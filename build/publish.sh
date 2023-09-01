081525c1590f62a700ba5333c7a3448fecdc6d71
tutu.tg.zhou <tutu.tg.zhou@hstong.com>
2023-09-01 13:59:17 +0800

    feat: add version '8.2.1'

--- build/publish.sh
+++ build/publish.sh
@@ -7,6 +7,6 @@ echo "查找到${name}的版本号：${version}"
 cd ..
 git add .
 git commit -m "feat: add version '$version'"
-git tag add -a $version -m "feat: add version '$version'"
+git tag -a $version -m "feat: add version '$version'"
 git push --tags
 pod trunk push *.podspec --verbose --use-libraries --allow-warnings --skip-import-validation --skip-tests
